84b03b4c4496c5d5da569a2c0c882fdb0984bfcc
GustavoW <guvieirawalter@gmail.com>
2025-08-22 18:09:42 -0300

    Criada pasta e projeto de testes

---
+++ ControleDeCinema.Testes.Integracao/ModuloSala/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeCinema.Testes.Integracao.ModuloSala
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void RepositorioSalaEmOrmTests()
+        {
+        }
+    }
+}
