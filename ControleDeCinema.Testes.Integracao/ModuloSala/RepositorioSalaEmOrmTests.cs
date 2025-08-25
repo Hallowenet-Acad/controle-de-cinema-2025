a64033aca8a70cde23c001e7d173f75e1ca4561f
GustavoW <guvieirawalter@gmail.com>
2025-08-25 19:08:27 -0300

    feat: Implementa Teste de Cadastro de Sala

--- ControleDeCinema.Testes.Integracao/ModuloSala/RepositorioSalaEmOrmTests.cs
+++ ControleDeCinema.Testes.Integracao/ModuloSala/RepositorioSalaEmOrmTests.cs
@@ -1,11 +1,55 @@
-﻿namespace ControleDeCinema.Testes.Integracao.ModuloSala
+﻿using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Infraestrutura.Orm.Compartilhado;
+using ControleDeCinema.Infraestrutura.Orm.ModuloSala;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloSala;
+
+[TestClass]
+[TestCategory("Testes de Integração de Sala")]
+public sealed class RepositorioSalaEmOrmTests
 {
-    [TestClass]
-    public sealed class RepositorioSalaEmOrmTests
+    private ControleDeCinemaDbContext dbContext;
+    private RepositorioSalaEmOrm repositorioSala;
+    
+    [TestInitialize]        
+    public void ConfigurarTestes()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        var assembly = typeof(RepositorioSalaEmOrmTests).Assembly;
+
+        var configuracao = new ConfigurationBuilder()
+            .AddUserSecrets(assembly)
+            .Build();
+
+        var connectionString = configuracao["SQL_CONNECTION_STRING"];
+
+        var options = new DbContextOptionsBuilder<ControleDeCinemaDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+
+        dbContext = new ControleDeCinemaDbContext(options);
+        repositorioSala = new RepositorioSalaEmOrm(dbContext);
+
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+    }
+
+    [TestMethod]
+    public void Deve_Cadastrar_Registro_Corretamente()
+    {
+        // Arrange
+        var sala = new Sala(3, 60);
+        
+        // Act
+        repositorioSala.Cadastrar(sala);
+
+        dbContext.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioSala.SelecionarRegistroPorId(sala.Id);
+
+        Assert.AreEqual(sala, registroSelecionado);
     }
 }
