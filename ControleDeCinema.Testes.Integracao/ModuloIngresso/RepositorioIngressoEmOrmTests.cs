81bf46121dc6fc236620f68bc5a2355d5fef2dae
GustavoW <guvieirawalter@gmail.com>
2025-08-25 20:50:08 -0300

    feat: Implementa Teste Cadastro de Registros Sessao (e arquivo Repositorio de Teste de Ingresso)

---
+++ ControleDeCinema.Testes.Integracao/ModuloIngresso/RepositorioIngressoEmOrmTests.cs
@@ -0,0 +1,44 @@
+﻿using ControleDeCinema.Dominio.ModuloSessao;
+using ControleDeCinema.Infraestrutura.Orm.Compartilhado;
+using ControleDeCinema.Infraestrutura.Orm.ModuloSala;
+using ControleDeCinema.Infraestrutura.Orm.ModuloSessao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloIngresso;
+
+[TestClass]
+[TestCategory("Testes de Integração de Ingresso")]
+public sealed class RepositorioIngressoEmOrmTests
+{
+    private ControleDeCinemaDbContext dbContext;
+    private RepositorioIngressoEmOrm repositorioIngresso;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        var assembly = typeof(RepositorioIngressoEmOrmTests).Assembly;
+
+        var configuracao = new ConfigurationBuilder()
+            .AddUserSecrets(assembly)
+            .Build();
+
+        var connectionString = configuracao["SQL_CONNECTION_STRING"];
+
+        var options = new DbContextOptionsBuilder<ControleDeCinemaDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+
+        dbContext = new ControleDeCinemaDbContext(options);
+        repositorioIngresso = new RepositorioIngressoEmOrm(dbContext);
+
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+    }
+
+    [TestMethod]
+    public void Deve_Cadastrar_Registro_Corretamente()
+    {
+    }
+
+}
---
+++ ControleDeCinema.Testes.Integracao/ModuloSessao/RepositorioSessaoEmOrmTests.cs
@@ -0,0 +1,54 @@
+﻿using ControleDeCinema.Dominio.ModuloSessao;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Infraestrutura.Orm.Compartilhado;
+using ControleDeCinema.Infraestrutura.Orm.ModuloSessao;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloSessao;
+
+[TestClass]
+[TestCategory("Testes de Integração de Sessão")]
+public sealed class RepositorioSessaoEmOrmTests
+{
+    private ControleDeCinemaDbContext dbContext;
+    private RepositorioSessaoEmOrm repositorioSessao;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        var assembly = typeof(RepositorioSessaoEmOrmTests).Assembly;
+
+        var configuracao = new ConfigurationBuilder()
+            .AddUserSecrets(assembly)
+            .Build();
+
+        var connectionString = configuracao["SQL_CONNECTION_STRING"];
+
+        var options = new DbContextOptionsBuilder<ControleDeCinemaDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+
+        dbContext = new ControleDeCinemaDbContext(options);
+        repositorioSessao = new RepositorioSessaoEmOrm(dbContext);
+
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+    }
+
+    [TestMethod]
+    public void Deve_Cadastrar_Registro_Corretamente()
+    {
+        var sessao = new Sessao(DateTime.UtcNow, 50, new Filme("Interestelar", 3, true, new GeneroFilme("Ficção Científica")), new Sala(1, 50));
+        
+        repositorioSessao.Cadastrar(sessao);
+
+        dbContext.SaveChanges();
+
+        var registroSelecionado = repositorioSessao.SelecionarRegistroPorId(sessao.Id);
+
+        Assert.AreEqual(sessao, registroSelecionado);
+    }
+}
