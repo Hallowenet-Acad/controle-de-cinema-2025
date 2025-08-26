1c5e16e0d951ffb5edf71c8cfc796c70528f7142
GustavoW <guvieirawalter@gmail.com>
2025-08-26 18:56:34 -0300

    feat: Implementa Teste Unitário Cadastro Correto Sessao

---
+++ ControleDeCinema.Testes.Unidade/ModuloSessao/SessaoAppServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using ControleDeCinema.Aplicacao.ModuloSessao;
+using ControledeCinema.Dominio.Compartilhado;
+using ControleDeCinema.Dominio.ModuloAutenticacao;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Dominio.ModuloSessao;
+using Microsoft.Extensions.Logging;
+using FizzWare.NBuilder;
+using Moq;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloSala;
+
+[TestClass]
+[TestCategory("Testes de Unidade de Sessao")]
+public sealed class SessaoAppServiceTests
+{
+    private Mock<ITenantProvider>? tenantProviderMock;
+    private Mock<IRepositorioSessao> repositorioSessaoMock;
+    private Mock<IUnitOfWork> unitOfWorkMock;
+    private Mock<ILogger<SessaoAppService>>? loggerMock;
+
+    private SessaoAppService? sessaoAppService;
+
+    [TestInitialize]
+    public void ConfigurarTeste()
+    {
+        tenantProviderMock = new Mock<ITenantProvider>();
+        repositorioSessaoMock = new Mock<IRepositorioSessao>();
+        unitOfWorkMock = new Mock<IUnitOfWork>();
+        loggerMock = new Mock<ILogger<SessaoAppService>>();
+        sessaoAppService = new SessaoAppService(
+            tenantProviderMock.Object,
+            repositorioSessaoMock.Object,
+            unitOfWorkMock.Object,
+            loggerMock.Object
+            );
+    }
+
+
+    [TestMethod]
+    public void Deve_Cadastrar_Sessao_Corretamente()
+    {
+        var genero = new GeneroFilme("Ação");
+        var filme = new Filme("Heat", 160, false, genero);
+        var sala = new Sala(1, 100);
+        var sessao = new Sessao(DateTime.Parse("05/07/2005 19:30:00"), 100, filme, sala);
+
+        repositorioSessaoMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<Sessao>() { });
+
+        var resultado = sessaoAppService!.Cadastrar(sessao);
+
+        repositorioSessaoMock?.Verify(r => r.Cadastrar(sessao), Times.Once);
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsSuccess);
+    }
+}
