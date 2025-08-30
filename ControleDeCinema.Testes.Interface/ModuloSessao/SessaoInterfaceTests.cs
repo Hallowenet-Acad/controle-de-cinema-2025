f8f9200da6569513b9fb09387aeaac917959e576
GustavoW <guvieirawalter@gmail.com>
2025-08-30 03:22:04 -0300

    feat: Implementa Teste de Interface de Edição de Sessao

--- ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTests.cs
+++ ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTests.cs
@@ -64,4 +64,72 @@ public sealed class SessaoInterfaceTests : TestFixture
         // Assert
         Assert.IsTrue(sessaoIndex.ContemSessao("John Wick", horario));
     }
+
+    [TestMethod]
+    public void Deve_Editar_Sessao()
+    {
+        // Arrange
+        //var authIndex = new AutentificacaoIndexPageObject(driver!)
+        //    .IrParaLogin(enderecoBase!)
+        //    .ClickCriarConta()
+        //    .PreencherEmail("cinema@gmail.com")
+        //    .PreencherSenha("Senha12345")
+        //    .PreencherConfirmarSenha("Senha12345")
+        //    .SelecionarTipoUsuario("Empresa")
+        //    .Confirmar();
+
+        //var generoFilmeIndex = new GeneroFilmeIndexPageObject(driver!)
+        //    .IrPara(enderecoBase!);
+
+        //generoFilmeIndex
+        //    .ClickCadastrar()
+        //    .PreencherDescricao("Ação")
+        //    .Confirmar();
+
+        //var filmeIndex = new FilmeIndexPageObject(driver!)
+        //    .IrPara(enderecoBase!);
+
+        //filmeIndex
+        //    .ClickCadastrar()
+        //    .PreencherTitulo("John Wick")
+        //    .PreencherDuracao(120)
+        //    .SelecionarGenero("Ação")
+        //    .Confirmar();
+
+        //var salaIndex = new SalaIndexPageObject(driver!)
+        //    .IrPara(enderecoBase!);
+
+        //salaIndex
+        //    .ClickCadastrar()
+        //    .PreencherNumeroSala(1)
+        //    .PreencherCapacidade(100)
+        //    .Confirmar();
+
+        var sessaoIndex = new SessaoIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        sessaoIndex
+            .ClickCadastrar()
+            .PreencherDataHorarioInicio(DateTime.Now.AddHours(5))
+            .PreencherNumeroMaximoIngressos(100)
+            .SelecionarFilme("John Wick")
+            .SelecionarSala(1)
+            .Confirmar();
+
+        // Act
+        var horario = DateTime.Now.AddHours(6);
+
+        sessaoIndex
+            .ClickEditar()
+            .PreencherDataHorarioInicio(horario)
+            .PreencherNumeroMaximoIngressos(90)
+            .SelecionarFilme("John Wick")
+            .SelecionarSala(1)
+            .Confirmar();
+
+        // Assert
+        Assert.IsTrue(sessaoIndex.ContemSessao("John Wick", horario));
+    }
+
+    
 }
