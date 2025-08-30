1f7b1e18a4ccde2a8f56cde15d1c19135928e8b3
GustavoW <guvieirawalter@gmail.com>
2025-08-29 21:41:55 -0300

    refactor: Separado PageObjects de Sala em Form e Index

---
+++ ControleDeCinema.Testes.Interface/ModuloSala/SalaIndexPageObject.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ControleDeCinema.Testes.Interface.ModuloSala;
+
+public class SalaIndexPageObject
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+    public SalaIndexPageObject(IWebDriver driver)
+    {
+        this.driver = driver;
+
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+
+    public SalaIndexPageObject IrPara(string enderecoBase)
+    {
+        driver.Navigate().GoToUrl(Path.Combine(enderecoBase, "salas"));
+
+        return this;
+    }
+    public SalaFormPageObject ClickCadastrar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']"))).Click();
+
+        return new SalaFormPageObject(driver!);
+    }
+
+    public SalaFormPageObject ClickEditar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector(".card a[title='Edição']"))).Click();
+
+        return new SalaFormPageObject(driver!);
+    }
+
+    public SalaFormPageObject ClickExcluir()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector(".card a[title='Exclusão']"))).Click();
+
+        return new SalaFormPageObject(driver!);
+    }
+
+    public bool ContemSala(string sala)
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']")).Displayed);
+
+        return driver.PageSource.Contains(sala);
+    }
+
+    public bool ChamouExcecaoDeNumero()
+    {
+        try
+        {
+            wait.Until(d => d.FindElement(By.CssSelector("span[data-se='spanNumero']")));
+            return true;
+        }
+        catch (WebDriverTimeoutException)
+        {
+            return false;
+        }
+    }
+
+    public bool ChamouAlert()
+    {
+        try
+        {
+            wait.Until(d => d.FindElement(By.CssSelector("div[data-se='alert']")));
+            return true;
+        }
+        catch (WebDriverTimeoutException)
+        {
+            return false;
+        }
+    }
+}
