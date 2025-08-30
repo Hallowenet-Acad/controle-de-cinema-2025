6e34f0bdbba0dae7fae4235d058fdcc2664571de
GustavoW <guvieirawalter@gmail.com>
2025-08-30 03:22:29 -0300

    feat: Implementa Teste de Interface de Exclusão de Sessao

--- ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTests.cs
+++ ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTests.cs
@@ -131,5 +131,70 @@ public sealed class SessaoInterfaceTests : TestFixture
         Assert.IsTrue(sessaoIndex.ContemSessao("John Wick", horario));
     }
 
-    
+    [TestMethod]
+    public void Deve_Excluir_Sessao()
+    {
+        // Arrange
+        //var authIndex = new AutentificacaoIndexPageObject(driver!)
+        //    .IrParaLogin(enderecoBase!)
+        //    .ClickCriarConta()
+        //    .PreencherEmail("cinema@gmail.com")
+        //    .PreencherSenha("Senha12345")
+        //    .PreencherConfirmarSenha("Senha12345")
+        //    .SelecionarTipoUsuario("Empresa")
+        //    .Confirmar();
+
+        //var generoFilmeIndex = new GeneroFilmeIndexPageObject(driver!)
+        //    .IrPara(enderecoBase!);
+
+        //generoFilmeIndex
+        //    .ClickCadastrar()
+        //    .PreencherDescricao("Ação")
+        //    .Confirmar();
+
+        //var filmeIndex = new FilmeIndexPageObject(driver!)
+        //    .IrPara(enderecoBase!);
+
+        //filmeIndex
+        //    .ClickCadastrar()
+        //    .PreencherTitulo("John Wick")
+        //    .PreencherDuracao(120)
+        //    .SelecionarGenero("Ação")
+        //    .Confirmar();
+
+        //var salaIndex = new SalaIndexPageObject(driver!)
+        //    .IrPara(enderecoBase!);
+
+        //salaIndex
+        //    .ClickCadastrar()
+        //    .PreencherNumeroSala(1)
+        //    .PreencherCapacidade(100)
+        //    .Confirmar();
+
+        var sessaoIndex = new SessaoIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        var horario = DateTime.Now.AddHours(5);
+
+        sessaoIndex
+            .ClickCadastrar()
+            .PreencherDataHorarioInicio(horario)
+            .PreencherNumeroMaximoIngressos(50)
+            .SelecionarFilme("John Wick")
+            .SelecionarSala(1)
+            .Confirmar();
+
+        // Act
+        sessaoIndex
+            .ClickEncerrar()
+            .Confirmar()
+            .IrPara(enderecoBase!);
+
+        sessaoIndex
+            .ClickExcluir()
+            .Confirmar();
+
+        // Assert
+        Assert.IsFalse(sessaoIndex.ContemSessao("John Wick", horario));
+    }
 }
