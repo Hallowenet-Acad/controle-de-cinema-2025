e691f24cd81209e0b11336d198bc53fe47a4e56f
RavoalM <alvarofeltrin2006@uniplaclages.edu.br>
2025-09-01 02:56:50 -0300

    feat: Implementa modulo Filme na camada de teste de interface

---
+++ ControleDeCinema.Testes.Interface/ModuloFilme/FilmeIndexPageObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace ControleDeCinema.Testes.Interface.ModuloFilme;
+
+public class FilmeIndexPageObject
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+    public FilmeIndexPageObject(IWebDriver driver)
+    {
+        this.driver = driver;
+
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        wait.IgnoreExceptionTypes(typeof(StaleElementReferenceException), typeof(NoSuchElementException));
+    }
+
+    public FilmeIndexPageObject IrPara(string enderecoBase)
+    {
+        driver.Navigate().GoToUrl($"{enderecoBase.TrimEnd('/')}/filmes");
+
+        wait.Until(d => d.Url.Contains("/filmes", StringComparison.OrdinalIgnoreCase));
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']")).Displayed);
+
+        return this;
+    }
+
+    public FilmeFormPageObject ClickCadastrar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']"))).Click();
+
+        return new(driver);
+    }
+
+    public FilmeFormPageObject ClickEditar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnEditar']"))).Click();
+
+        return new(driver);
+    }
+
+    public FilmeFormPageObject ClickExcluir()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnExcluir']"))).Click();
+
+        return new(driver);
+    }
+
+    public bool ContemFilme(string titulo)
+    {
+        return driver.PageSource.Contains(titulo);
+    }
+}
---
+++ ControleDeCinema.Testes.Interface/ModuloFilme/FilmeInterfaceTest.cs
@@ -0,0 +1,238 @@
+﻿using ControleDeCinema.Testes.Interface.Compartilhado;
+using ControleDeCinema.Testes.Interface.ModuloGenero;
+
+namespace ControleDeCinema.Testes.Interface.ModuloFilme;
+
+[TestClass]
+[TestCategory("Testes de Interface de Filme")]
+public sealed class FilmeInterfaceTestes : TestFixture
+{
+    [TestInitialize]
+    public override void InicializarTeste()
+    {
+        base.InicializarTeste();
+
+        RegistrarContaEmpresarial();
+    }
+
+    [TestMethod]
+    public void Deve_Cadastrar_Filme_Corretamente()
+    {
+        GeneroIndexPageObject generoIndexPage = new(driver);
+
+        generoIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Terror")
+        .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndexPage = new(driver);
+
+        FilmeFormPageObject filmeFormPage = filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar();
+
+        filmeFormPage
+            .PreencherTitulo("A Hora do Mal")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Terror")
+            .ClickSubmit();
+
+        Assert.IsTrue(filmeIndexPage.ContemFilme("A Hora do Mal"));
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Filme_Corretamente()
+    {
+
+        GeneroIndexPageObject generoIndexPage = new(driver);
+
+        generoIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Terror")
+        .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndexPage = new(driver);
+
+        filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("A Hora do Mal")
+            .PreencherDuracao(125)
+            .MarcarLancamento()
+            .SelecionarGenero("Terror")
+            .ClickSubmit();
+
+        FilmeFormPageObject filmeFormPage = filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickEditar();
+
+        filmeFormPage
+            .PreencherTitulo("A Hora do Bem")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Religião")
+            .ClickSubmit();
+
+        Assert.IsTrue(filmeIndexPage.ContemFilme("A Hora do Bem"));
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Filme_Corretamente()
+    {
+        GeneroIndexPageObject generoIndexPage = new(driver);
+
+        generoIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Terror")
+        .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndexPage = new(driver);
+
+        filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("Longlegs")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Terror")
+            .ClickSubmit();
+
+        FilmeFormPageObject filmeFormPage = filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickExcluir();
+
+        filmeFormPage
+            .ClickSubmitExcluir("Longlegs");
+
+        Assert.IsFalse(filmeIndexPage.ContemFilme("Longlegs"));
+    }
+
+    [TestMethod]
+    public void Deve_Visualizar_Filmes_Cadastrados_Corretamente()
+    {
+        GeneroIndexPageObject generoIndexPage = new(driver);
+
+        generoIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Terror")
+            .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndexPage = new(driver);
+
+        filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("Pânico")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Terror")
+            .ClickSubmit();
+
+        filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("Nosferatu")
+            .PreencherDuracao(135)
+            .MarcarLancamento()
+            .SelecionarGenero("Romance")
+            .ClickSubmit();
+
+        filmeIndexPage
+            .IrPara(enderecoBase);
+
+        Assert.IsTrue(filmeIndexPage.ContemFilme("Pânico"));
+        Assert.IsTrue(filmeIndexPage.ContemFilme("Nosferatu"));
+    }
+
+    [TestMethod]
+    public void Nao_Deve_Cadastrar_Filme_Com_Campos_Vazios()
+    {
+        GeneroIndexPageObject generoIndexPage = new(driver);
+
+        generoIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Terror")
+            .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndexPage = new(driver);
+
+        FilmeFormPageObject filmeFormPage = filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar();
+
+        filmeFormPage
+            .ClickSubmitEsperandoErros();
+
+        Assert.IsTrue(filmeFormPage.EstourouValidacao("Titulo"));
+        Assert.IsTrue(filmeFormPage.EstourouValidacao("Duracao"));
+    }
+
+    [TestMethod]
+    public void Nao_Deve_Cadastrar_Filme_Com_Duracao_Invalida()
+    {
+        GeneroIndexPageObject generoIndexPage = new(driver);
+
+        generoIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Terror")
+            .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndexPage = new(driver);
+
+        FilmeFormPageObject filmeFormPage = filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar();
+
+        filmeFormPage
+            .PreencherTitulo("Esposa de Mentirinha")
+            .PreencherDuracao(0)
+            .MarcarLancamento()
+            .SelecionarGenero("Comédia")
+            .ClickSubmitEsperandoErros();
+
+        Assert.IsTrue(filmeFormPage.EstourouValidacao("Duracao"));
+    }
+
+    [TestMethod]
+    public void Nao_Deve_Cadastrar_Filme_Com_Titulo_Duplicado()
+    {
+        GeneroIndexPageObject generoIndexPage = new(driver);
+
+        generoIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Terror")
+            .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndexPage = new(driver);
+
+        filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("Faça Ela Voltar")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Terror")
+            .ClickSubmit();
+
+        FilmeFormPageObject filmeFormPage = filmeIndexPage
+            .IrPara(enderecoBase)
+            .ClickCadastrar();
+
+        filmeFormPage
+            .PreencherTitulo("Faça Ela Voltar")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Terror")
+            .ClickSubmitEsperandoErros();
+
+        Assert.IsTrue(filmeFormPage.EstourouValidacao());
+    }
+}
